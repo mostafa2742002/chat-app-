d27b1e618c60c8708130e321669e2d738132290a
Mostafa Mahmoud <mostafa19500mahmoud@gmail.com>
2024-04-05 02:05:37 +0200

    Refactor chat message broadcasting and update socket connection URL

--- app.js
+++ app.js
@@ -11,21 +11,45 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/web.html');
 });
 
+// datastructure to store connected clients
+let clients = [];
+
 // Socket.IO connection handling
 io.on('connection', (socket) => {
   console.log('A user connected');
+  
+  // Function to broadcast updated user list to all clients
+  const broadcastUserList = () => {
+    io.emit('get_users', clients);
+  };
+
+  // When a new user connects
+  socket.on("username", (username) => {
+    console.log('Username: ' + username);
+    socket.username = username;
+    clients.push(username);
+    broadcastUserList();
+    // Broadcast to all connected users that a new user has joined
+    socket.broadcast.emit('is_online', socket.username);
+  });
 
   // Listen for chat messages
   socket.on('chat message', (msg) => {
     console.log('Message: ' + msg);
-
-    // Broadcast the message to all connected clients except the sender
-    socket.broadcast.emit('chat message', msg);
+    // Broadcast the message to all connected clients
+    io.emit('chat message', msg);
   });
 
   // Handle disconnection
   socket.on('disconnect', () => {
-    console.log('User disconnected');
+    if (socket.username) {
+      // Broadcast to all connected users that a user has left
+      io.emit('user_disconnected', socket.username);
+      // Remove the disconnected user from the list
+      clients = clients.filter((client) => client !== socket.username);
+      broadcastUserList();
+      console.log('User disconnected');
+    }
   });
 });
 
